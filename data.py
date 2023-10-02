947ac76fff6162f0da733409ddb9417b2e865756
jessiejessiewang <jessiewang0907@gmail.com>
2023-10-02 19:26:02 +0800

    upload MPP project

--- data.py
+++
@@ -1,8 +0,0 @@
-import os.path
-
-from joblib import Memory
-from xbbg import blp
-
-location = os.path.dirname(os.path.realpath(__file__)) + '/cachedir'
-memory = Memory(location, verbose=0)
-bdh = memory.cache(blp.bdh)
